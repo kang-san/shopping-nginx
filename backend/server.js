7c27104f848e7a6fdc87604f6011975dd01ffe34
kangsan <junbuck01@naver.com>
2022-07-01 13:43:14 +0900

    Merge remote-tracking branch 'origin/master'

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: true}));
 
 //CORS 미들웨어 설정
 const corsOptions  = {
-    origin: process.env.CLIENT_ORIGEN || "http://localhost:3000"
+    origin: process.env.CLIENT_ORIGEN || "http://172.31.93.123:3000"
 };
 
 console.log("CLIENT_ORIGIN     " + process.env.CLIENT_ORIGIN)
@@ -51,5 +51,5 @@ const PORT = process.env.NODE_DOCKER_PORT || 5000;
 console.log("NODE_DOCKER_PORT     " + process.env.NODE_DOCKER_PORT)
 
 app.listen(PORT, () =>{
-    console.log(`Server at http://localhost:${PORT}`);
+    console.log(`Server at http://172.31.93.123:${PORT}`);
 });
